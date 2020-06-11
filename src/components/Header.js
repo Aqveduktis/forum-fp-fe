363572b5f19a9fb01d7e2c571f65c4e801f4d8fe
sara <sara.lovens.lovn@gmail.com>
2020-06-11 09:17:55 +0200

    adding loading animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import {useSelector} from 'react-redux'
+import styled, {keyframes} from 'styled-components';
 import {StyledLink} from '../lib/shared'
 import { Title } from 'lib/text';
 import { Banner } from 'lib/containers';
@@ -16,19 +17,39 @@ margin: 10px 0;
   color: silver;
 }
 `
+const colorChanging = keyframes`
+0%   {color: white;}
+50%  {color: black;}
+100% {color: white;}
+
+`
+
+
+
+const Loading = styled.p`
+font-size: 20px;
+font-width: bold;
+animation: ${colorChanging} 5s linear infinite;
+
+`
 
 
 export const Header = () => {
+	const isLoading = useSelector((state)=>state.userStore.isLoading)
 	return (
 		<>
     <Banner>
       <Title to="/">My Forum</Title>
 			<UserLogo size={100} />
     </Banner>
+		<Banner>
 		<LinkNav>
 				<StyledLink to="/user">User</StyledLink>
 				<StyledLink to="/games">About</StyledLink>
 			</LinkNav>
+			{isLoading && <Loading>Loading</Loading>}
+    </Banner>
+	
     <StatusMessages />
 		<Result />
 		</>
