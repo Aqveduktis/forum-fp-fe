62d5318ef6dfe5ca91f3586ef4232c0851d1ced9
sara <sara.lovens.lovn@gmail.com>
2020-06-11 08:49:16 +0200

    adding logo for user

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import { Title } from 'lib/text';
 import { Banner } from 'lib/containers';
 import {Result} from '../components/Result'
 import { StatusMessages } from './StatusMessages';
+import { UserLogo } from 'lib/UserLogo';
 
 
 const LinkNav = styled.nav`
@@ -22,11 +23,12 @@ export const Header = () => {
 		<>
     <Banner>
       <Title to="/">My Forum</Title>
-    			<LinkNav>
+			<UserLogo size={100} />
+    </Banner>
+		<LinkNav>
 				<StyledLink to="/user">User</StyledLink>
 				<StyledLink to="/games">About</StyledLink>
 			</LinkNav>
-    </Banner>
     <StatusMessages />
 		<Result />
 		</>
