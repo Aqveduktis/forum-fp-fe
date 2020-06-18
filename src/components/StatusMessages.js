eff5b3c123c7696a6fe61eadadab7dcc89d1b696
sara <sara.lovens.lovn@gmail.com>
2020-06-18 07:47:34 +0200

    adding set timeout

--- src/components/StatusMessages.js
+++ src/components/StatusMessages.js
@@ -3,14 +3,17 @@ import { useSelector } from 'react-redux';
 import { Text } from 'lib/text';
 
 export const StatusMessages = () => {
-  const errorMessage = useSelector((state)=>state.statusStore.errorMessage)
-  const statusMessage = useSelector((state)=>state.statusStore.statusMessage)
+	const errorMessage = useSelector((state) => state.statusStore.errorMessage);
+	const statusMessage = useSelector((state) => state.statusStore.statusMessage);
+
+	// window.setTimeout(function() {
+	// 	alert('Hello World!');
+	// }, 500);
+
 	return (
 		<div>
-		<Text color="palevioletred">{errorMessage}</Text>
+			<Text color="palevioletred">{errorMessage}</Text>
 			<Text color="palegreen">{statusMessage}</Text>
 		</div>
 	);
 };
-	
-			
